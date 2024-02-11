73e1834df21dc0ece59ede22e32d3c1f83ceeafb
Nic Rosental <nicrosental@gmail.com>
2024-02-11 13:57:59 -0500

    conditioned button, fixed up service namespacing.

--- app/Actions/contacts/AugmentWithLinkedIn.php
+++ app/Actions/contacts/AugmentWithLinkedIn.php
@@ -3,15 +3,17 @@
 namespace App\Actions\Contacts;
 
 use App\Models\Contact;
-use App\Services\ScraperApiService\ScraperApiService;
+use App\Services\ScraperApiService;
 
 class AugmentWithLinkedIn
 {
     public function __invoke(Contact $contact)
     {
         $client = new ScraperApiService(config('services.scraperapi.api_key'));
+        
         $client->get([
-            'https://www.linkedin.com/in/'.$contact->linkedin,
+            // 'https://www.linkedin.com/in/'.$contact->linkedin,
+            $contact->linkedin,
         ]);
     }
 }
--- resources/views/livewire/contacts/show.blade.php
+++ resources/views/livewire/contacts/show.blade.php
@@ -21,7 +21,7 @@
             </span>
             <span class="my-4 mr-2">
             <button wire:click="augmentWithLinkedIn"
-                class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mr-2 border border-blue-500 hover:border-transparent rounded">
+                class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mr-2 border border-blue-500 hover:border-transparent rounded disabled:text-blue-200 disabled:hover:text-blue-200 disabled:hover:bg-blue-300" @if(!$contact->linkedin) disabled title="Provide LinkedId profile to use this feature" @endif>
                 Augment with LinkedIn</button>
             </span>
             {{-- <button
